3747f86543b0aeaf337384d48caa642ff4db9391
Anthony Shin <shinn92dev@gmail.com>
2025-01-05 19:37:56 -0800

    pointer change

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -14,14 +14,19 @@ const TopBar: React.FunctionComponent = () => {
     };
     return (
         <div className="top-bar w-full max-w-md flex justify-between p-4 bg-white shadow-md">
-            <div className="text-2xl font-bold" onClick={handleHomeClick}>
+            <div
+                className="text-2xl font-bold cursor-pointer"
+                onClick={handleHomeClick}
+            >
                 RoundUp
             </div>
             <div className="flex items-center">
                 <div className="mr-5">
                     {isSignedIn ? <UserButton /> : <></>}
                 </div>
-                <SettingIcon />
+                <div className="cursor-pointer">
+                    <SettingIcon />
+                </div>
             </div>
         </div>
     );
