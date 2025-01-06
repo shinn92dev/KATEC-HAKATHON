12b77e602b3d0f0118ecb87720987511795a2fe3
Saeyoung Park <spark363@my.bcit.ca>
2025-01-05 17:25:44 -0800

    Add profile page

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,15 +1,40 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
 const UserProfile: React.FC = () => {
-    return (
-        <div className="profile content">
-            User name <br />
-            Email address <br />
-            Paypal account ID <br />
-            Preferred organization <br />
-            Preferred field <br />
-        </div>
-    );
+
+    const [isConnected] = useState<boolean>(() => {
+            const savedState = localStorage.getItem("isConnected");
+            return savedState === "true"; 
+        });
+
+        const { user } = useUser();
+        const userName = user?.fullName || user?.id || user?.externalId;
+        const userEmail = user?.emailAddresses.toString() || user?.id
+        
+
+        return (
+            <div className="logged-out-page text-left tracking-wider leading-loose content flex justify-center">
+                {isConnected ? (
+                    <div className="ext-lg text-gray-800 mb-6">
+                            Name: {userName} <br />
+                            Email: {userEmail} <br />
+                            Paypal status: Connected <br />
+            
+                    </div>
+                ) : (
+                    <>
+                        <div className="ext-lg text-gray-800 mb-6">
+                                Name: {userName} <br />
+                                Email: {userEmail} <br />
+                                Paypal status: Not connected <br />
+
+                        </div>
+                    </>
+                )}
+            </div>
+        );
+
 };
 
 export default UserProfile;
